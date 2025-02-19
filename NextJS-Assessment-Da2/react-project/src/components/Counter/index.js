401bcd1ac0ddfdba74151d64e45147a273c6a8b4
System Administrator <root@Milans-MacBook-Pro.local>
2025-02-19 12:22:06 +0530

    worked on day 2

--- NextJS-Assessment-Da2/react-project/src/components/Counter/index.js
+++ NextJS-Assessment-Da2/react-project/src/components/Counter/index.js
@@ -3,8 +3,12 @@ import { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  const handleDecrement = () => setCount(count - 1);
-  const handleIncrement = () => setCount(count + 1)
+  const handleDecrement = () => {
+      count > 0 && (setCount(count - 1))
+  };
+  const handleIncrement = () => {
+    setCount(count + 1)
+  }
 
   return (
     <div className="container">
@@ -12,12 +16,11 @@ const Counter = () => {
        <h3>Q1. Create a counter component with a button. <br/>
        Q2. Create a counter with increment and decrement buttons.
        </h3>
-      <button className="" onClick={handleDecrement}>
+      <button onClick={handleDecrement}>
         Decrement
       </button>
       <strong className="count">Counter: {count}</strong>
-      <button
-        className="" onClick={handleIncrement}>
+      <button onClick={handleIncrement}>
         Increment
       </button>
       </div>
--- NextJS-Assessment-Da2/react-project/src/components/TodoList/index.js
+++ NextJS-Assessment-Da2/react-project/src/components/TodoList/index.js
@@ -1,11 +1,34 @@
 import { useState } from "react";
 
 const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputTodo, setInputTodo] = useState("");
 
+  const handleTodo = () => {
+    if (inputTodo.trim()) {
+      setTodos([...todos, inputTodo]);
+      setInputTodo("");
+    }
+  };
   return (
     <div className="container">
       <div className="mt-50">
        <h3>Q4. Create a TodoList component that stores a list of to-dos in its state.</h3>
+
+       <input 
+        type="text" 
+        value={inputTodo} 
+        onChange={(e) => setInputTodo(e.target.value)} 
+        placeholder="todo's"
+      />
+      <button onClick={handleTodo}>
+        Add Todo
+      </button>
+      <ul className="mt-4 list-disc pl-5">
+        {todos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
       </div>
     </div>
   );
