d9bf941abb398a959cf343b303082cab0e0e1a66
System Administrator <root@Milans-MacBook-Pro.local>
2025-02-21 14:40:13 +0530

    worked on next js

--- NextJS-Assessment-Day4/next-page-router/src/pages/components/Footer/index.js
+++ NextJS-Assessment-Day4/next-page-router/src/pages/components/Footer/index.js
@@ -2,20 +2,20 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-  <div className="footer min-h-[500px] border-t-2 border-[#f2f2f2] bg-[#282828] text-[#ffffff] py-5">
-    <h3 className="text-center mb-20 text-[50px] font-extrabold">Footer Heading</h3>
-    <div className="container flex justify-center">
-    <nav className="flex">
-        <ul className="flex space-x-14 justify-center text-[#fff]">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/Recipies">Recipies</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="footer border-t-2 min-h-[500px]  border-[#f2f2f2] bg-[#282828] text-[#ffffff] py-5">
+      <h3 className="text-center mb-20 text-[50px] font-extrabold">Footer Heading</h3>
+      <div className="container flex justify-center">
+        <nav className="flex">
+          <ul className="flex space-x-14 text-[#fff]">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/Products">Products</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
-  </div>
   );
 }
